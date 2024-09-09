7c794d8341df17567f7f5ab2e32c8c0040722781
Valerii <berezovskijvalera812@gmail.com>
2024-09-09 16:05:32 +0200

    homework

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-document.write("Caramba!");
+document.write("Caramba!<br>");
+document.write("Berezovskiy Valera Vyacheslavovich<br>");
+document.write("ITSTEP<br>");
+document.write("Programing and Sport!");
