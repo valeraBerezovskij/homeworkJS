1b7786483d4dcc89a152b0646f6129b82226adfa
Valerii <berezovskijvalera812@gmail.com>
2024-09-25 21:39:19 +0200

    homework 9.25.24 { arrays }

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
 
-simpleFunction();
-console.log("Куб числа " + 3 + " равен " + cube(3) + "\n");
-console.log("Куб числа " + 5 + " равен " + cube(5) + "\n");
-console.log(cube);
-simpleFunction = null;
